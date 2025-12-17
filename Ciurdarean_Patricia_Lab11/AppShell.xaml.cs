b28f4f4d0370a70abfacd7225428049797a32316
PATRICIA CIURDĂREAN <patricia.ciurdarean@stud.ubbcluj.ro>
2025-12-17 22:37:36 +0200

    Add project files.

---
+++ Ciurdarean_Patricia_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ciurdarean_Patricia_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
